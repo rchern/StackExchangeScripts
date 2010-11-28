cad793787f3d17241ccc2df5813d802c2321821e
Tim Stone <tmslft@gmail.com>
2010-11-27 19:13:34 -0500

    Fixed comment highlighting stuff to be more reliable, relying on a callback instead of a timed delay.

--- SEModifications.user.js
+++ SEModifications.user.js
@@ -38,6 +38,52 @@ with_jquery(function ($) {
 		// add timeline and history links to post menu
 		var questionURL = $("#question-header a").attr("href");
 		if (questionURL) {
+			var needsHighlight = false;
+
+			function linkifyComments(reference, url) {
+				reference.find('.comment')
+					.each(function() {
+						$('.comment-date', this).wrap('<a href="' + url.replace(/#.*/, '#' + this.id) + '" />');
+					});
+			}
+
+			function highlightComment(comment) {
+				if(!(comment = $(comment)).length) {
+					return false;
+				}
+
+				needsHighlight = false;
+
+				$(document).scrollTop(comment.offset().top);
+
+				comment.css({ 'opacity': 0 })
+					.animate({ 'opacity': 1 }, 1500, 'linear');
+
+				return true;
+			}
+
+			$(document).ajaxComplete(function(event, request, options) {
+				if (options) {
+					var id = options.url.match(/^\/posts\/([0-9]+)\/comments/);
+
+					if (id) {
+						id = id[1];
+
+						if (needsHighlight) {
+							highlightComment(location.hash);
+						}
+						
+						var post = $('#comments-' + id);
+						var url = $('#question #comments-' + id).length ? questionURL + '#' :
+							post.closest('.answer, #question')
+								.find(".post-menu a:contains('link'):first")
+								.attr("href");
+
+						linkifyComments(post, url);
+					}
+				}
+			});
+
 			var post = questionURL.replace("questions", "posts").replace(/\/[^\/]*$/, ""),
 				timeline = post + "/timeline",
 				revisions = post + "/revisions";
@@ -54,215 +100,183 @@ with_jquery(function ($) {
 				}
 
 				$(this).append("<span class='lsep'>|</span><a href='" + revisions + "'>history</a>");
-				
-				// add comment link to comment date
-				$(this).closest('.answer, #question')
-					.find('.comment')
-					.each(function() {
-						$('.comment-date', this).wrap('<a href="' + postLink.replace(/#.*/, '#' + this.id) + '" />');
-					})
-					.end()
-					.find('.comments-link')
-					.click(function(event) {
-						var self = $(this);
 
-						// This isn't ideal...
-						setTimeout(function() {
-							self.prevAll('.comments')
-								.find('.comment')
-								.each(function() {
-									$('.comment-date', this).wrap('<a href="' + postLink.replace(/#.*/, '#' + this.id) + '" />');
-								});
-						}, 500);
-					});
+				linkifyComments($(this).closest('.answer, #question'), postLink);
 
 				revisions = null;
 			});
-		}
-		
-		// try our best to show a linked comment
-		var commentID = location.hash.match(/#(comment-[0-9]+)/),
-			comment;
 
-		if (commentID && !(comment = $(commentID[0])).length) {
-			// comment doesn't exist or it's hidden (more likely)
-			var post = location.pathname;
-				post = post.substring(post.lastIndexOf('/') + 1);
+			// try our best to show a linked comment
+			var commentID = location.hash.match(/#(comment-[0-9]+)/);
 
-			$((post.match(/[0-9]+/) ? '#answer-' + post : '#question') + ' .comments-link').click();
+			if (commentID && !highlightComment(commentID[0])) {
+				// comment doesn't exist or it's hidden (more likely)
+				var post = location.pathname;
+					post = post.substring(post.lastIndexOf('/') + 1);
 
-			// Again, not ideal...
-			setTimeout(function() {
-				var comment;
+				needsHighlight = true;
 
-				if ((comment = $(commentID[0])).length) {
-					$(document).scrollTop(comment.offset().top);
-					comment.css({ 'background-color': $('<span class="newuser" />').css('background-color') })
-						.animate({ 'background-color': $('#question').css('background-color') }, 2000, 'linear');
+				$((post.match(/^[0-9]+$/) ? '#answer-' + post : '#question') + ' .comments-link').click();
+			}
+
+			// add autocomplete to comments
+			$(document).bind('keydown', function(event) {
+				if (!event.shiftKey || event.which != 50)
+					return;
+
+				var target = $(event.target);
+
+				if (target.attr('name') == 'comment' && !target.hasClass('easy-auto-complete')) {
+					var isActive = false,
+						users = [];
+
+					function simplify(name) {
+						return name.clone().find('span').remove().end().text().replace(/\s+/g, '');
+					}
+
+					function autocomplete(event) {
+						var self = $(this),
+							text = self.val(),
+							matches,
+							list,
+							selectionIndex;
+
+						if (this.selectionStart != this.selectionEnd || event.which == 9) {
+							return;
+						}
+
+						selectionIndex = text.indexOf(' ', this.selectionEnd);
+						selectionIndex = selectionIndex == -1 ? text.length : selectionIndex;
+
+						if (isActive = ((matches = text.substring(0, selectionIndex).match(/@([^\s]+)$/))
+							&& (selectionIndex - matches[0].length == text.indexOf('@')))) {
+							(list = self.prev('.easy-auto-complete-matches'))
+								.children()
+								.remove();
+
+							$.each(users, function(index, value) {
+								if (value.toLowerCase().indexOf(matches[1].toLowerCase()) == 0) {
+									$('<li />').css({
+											'display': 'inline-block',
+											'background-color': '#FFFFFF',
+											'padding': '2px 4px 2px 4px',
+											'margin-right': '5px',
+											'cursor': 'pointer',
+											'border': '1px solid #888888',
+											'font-weight': value.toLowerCase() == matches[1].toLowerCase() ? 'bold' : 'normal'
+										})
+										.addClass(value.toLowerCase() == matches[1].toLowerCase() ? 'selected' : '')
+										.text(value)
+										.click(function() {
+											var text = $(this).text(),
+												value = self.val(),
+												box = self.focus()
+													.val(value.replace(/@[^\s]+/, '@' + text))[0];
+
+											box.selectionStart = box.selectionEnd = value.indexOf('@') + text.length + 1;
+
+											self.keyup();
+
+											return false;
+										})
+										.appendTo(list);
+								} else if (matches[1] < value) {
+									return false;
+								}
+							});
+
+							list.css({
+									'display': 'block',
+									'top': (self.offset().top - list.outerHeight(true)) + 'px',
+									'left': self.offset().left + 'px'
+								});
+						} else {
+							self.prev('.easy-auto-complete-matches')
+								.css({ 'display': 'none' })
+								.children()
+								.remove();
+						}
+					}
+
+					target.addClass('easy-auto-complete')
+						.before('<ul class="easy-auto-complete-matches" />')
+						.prev()
+						.css({
+							'display': 'none',
+							'position': 'absolute',
+							'width': target.width() + 'px',
+							'margin-left': '0px',
+							'margin-bottom': '5px',
+						})
+						.end()
+						.bind('keydown', function(event) {
+							var self = $(this);
+
+							if (isActive && event.which == 9) {
+								var matches = self.prev(),
+									selected = matches.find('.selected'),
+									next = selected.next(),
+									current;
+
+								if (selected.length) {
+									selected.removeClass('selected')
+										.css({ 'font-weight': 'normal' });
+
+									if (next.length) {
+										current = next.addClass('selected')
+											.css({ 'font-weight': 'bold' });
+									}
+								}
+
+								if (!next.length) {
+									current = matches.children()
+										.first()
+										.addClass('selected')
+										.css({ 'font-weight': 'bold' });
+								}
+
+								if (current && current.length) {
+									var text = current.text(),
+										value = self.val(),
+										box = self.focus()
+											.val(value.replace(/@[^\s]+/, '@' + text))[0];
+
+									box.selectionStart = box.selectionEnd = value.indexOf('@') + text.length + 1;
+								}
+
+								return false;
+							}
+						})
+						.bind('keyup click', autocomplete)
+						.data('_easy-auto-complete-users', users)
+						.closest('.comments')
+						.find('.comment-user')
+						.each(function() {
+							var username = simplify($(this));
+
+							if ($.inArray(username, users) == -1) {
+								users.push(username);
+							}
+						})
+						.closest('#question')
+						.find('.post-signature .user-details')
+						.each(function() {
+							var username = simplify($(this).find('a:first'));
+
+							if ($.inArray(username, users) == -1) {
+								users.push(username);
+							}
+						})
+						.trigger('keypress', function(event) {
+							users.sort();
+
+							return event;
+						}(event));
 				}
-			}, 500);
-		} else if (comment) {
-			comment.css({ 'background-color': $('<span class="newuser" />').css('background-color') })
-				.animate({ 'background-color': $('#question').css('background-color') }, 2000, 'linear');
+			});
 		}
 
 		// adds an audit link next to your rep in the header that leads to /reputation
 		$("#hlinks-user .reputation-score").parent().after("<a href='/reputation'>(audit)</a>");
-		
-		// add autocomplete to comments
-		$(document).bind('keydown', function(event) {
-			if (!event.shiftKey || event.which != 50)
-				return;
-
-			var target = $(event.target);
-
-			if (target.attr('name') == 'comment' && !target.hasClass('easy-auto-complete')) {
-				var isActive = false,
-					users = [];
-
-				function simplify(name) {
-					return name.clone().find('span').remove().end().text().replace(/\s+/g, '');
-				}
-
-				function autocomplete(event) {
-					var self = $(this),
-						text = self.val(),
-						matches,
-						list,
-						selectionIndex;
-
-					if (this.selectionStart != this.selectionEnd || event.which == 9) {
-						return;
-					}
-					
-					selectionIndex = text.indexOf(' ', this.selectionEnd);
-					selectionIndex = selectionIndex == -1 ? text.length : selectionIndex;
-
-					if (isActive = ((matches = text.substring(0, selectionIndex).match(/@([^\s]+)$/))
-						&& (selectionIndex - matches[0].length == text.indexOf('@')))) {
-						(list = self.prev('.easy-auto-complete-matches'))
-							.children()
-							.remove();
-
-						$.each(users, function(index, value) {
-							if (value.toLowerCase().indexOf(matches[1].toLowerCase()) == 0) {
-								$('<li />').css({
-										'display': 'inline-block',
-										'background-color': '#FFFFFF',
-										'padding': '2px 4px 2px 4px',
-										'margin-right': '5px',
-										'cursor': 'pointer',
-										'border': '1px solid #888888',
-										'font-weight': value.toLowerCase() == matches[1].toLowerCase() ? 'bold' : 'normal'
-									})
-									.addClass(value.toLowerCase() == matches[1].toLowerCase() ? 'selected' : '')
-									.text(value)
-									.click(function() {
-										var text = $(this).text(),
-											value = self.val(),
-											box = self.focus()
-												.val(value.replace(/@[^\s]+/, '@' + text))[0];
-
-										box.selectionStart = box.selectionEnd = value.indexOf('@') + text.length + 1;
-										
-										self.keyup();
-
-										return false;
-									})
-									.appendTo(list);
-							} else if (matches[1] < value) {
-								return false;
-							}
-						});
-
-						list.css({
-								'display': 'block',
-								'top': (self.offset().top - list.outerHeight(true)) + 'px',
-								'left': self.offset().left + 'px'
-							});
-					} else {
-						self.prev('.easy-auto-complete-matches')
-							.css({ 'display': 'none' })
-							.children()
-							.remove();
-					}
-				}
-
-				target.addClass('easy-auto-complete')
-					.before('<ul class="easy-auto-complete-matches" />')
-					.prev()
-					.css({
-						'display': 'none',
-						'position': 'absolute',
-						'width': target.width() + 'px',
-						'margin-left': '0px',
-						'margin-bottom': '5px',
-					})
-					.end()
-					.bind('keydown', function(event) {
-						var self = $(this);
-
-						if (isActive && event.which == 9) {
-							var matches = self.prev(),
-								selected = matches.find('.selected'),
-								next = selected.next(),
-								current;
-
-							if (selected.length) {
-								selected.removeClass('selected')
-									.css({ 'font-weight': 'normal' });
-
-								if (next.length) {
-									current = next.addClass('selected')
-										.css({ 'font-weight': 'bold' });
-								}
-							}
-
-							if (!next.length) {
-								current = matches.children()
-									.first()
-									.addClass('selected')
-									.css({ 'font-weight': 'bold' });
-							}
-							
-							if (current && current.length) {
-								var text = current.text(),
-									value = self.val(),
-									box = self.focus()
-										.val(value.replace(/@[^\s]+/, '@' + text))[0];
-
-								box.selectionStart = box.selectionEnd = value.indexOf('@') + text.length + 1;
-							}
-
-							return false;
-						}
-					})
-					.bind('keyup click', autocomplete)
-					.data('_easy-auto-complete-users', users)
-					.closest('.comments')
-					.find('.comment-user')
-					.each(function() {
-						var username = simplify($(this));
-
-						if ($.inArray(username, users) == -1) {
-							users.push(username);
-						}
-					})
-					.closest('#question')
-					.find('.post-signature .user-details')
-					.each(function() {
-						var username = simplify($(this).find('a:first'));
-
-						if ($.inArray(username, users) == -1) {
-							users.push(username);
-						}
-					})
-					.trigger('keypress', function(event) {
-						users.sort();
-						
-						return event;
-					}(event));
-			}
-		});
 	});
 });
